2d31eea6ad2a2a4302af067a6e26c153472a23fd
rvral-nxrd <ostv@mail.com>
2025-08-29 01:34:59 +0200

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -60,7 +60,7 @@ After=network.target
 
 [Service]
 User=$(whoami)
-ExecStart=/bin/bash -c '/usr/bin/inotifywait -m $DIRECTORY -e close_write --format "%w%f" 2>&1 | tee -a /var/log/transcode/inotify.log | while read -r filename; do /transcode/scripts/transcode.sh "$DIRECTORY/$filename"; done'
+ExecStart=/bin/bash -c '/usr/bin/inotifywait -m $DIRECTORY -e close_write --format "%w%f" 2>&1 | tee -a /var/log/transcode/inotify.log | while read -r filename; do if [ -n "$filename" ]; then /transcode/scripts/transcode.sh "$DIRECTORY$filename"; fi; done'
 Restart=always
 
 [Install]
